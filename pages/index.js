469cfa2f212e1f1e3be9a7ffc6847fc89cc0609b
iamidlek <iamidlek@naver.com>
2021-12-30 16:55:39 +0900

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ hello }) {
   return (
     <div className={styles.container}>
-      <h1>Blog Home</h1>
+      <h1>Blog Home {hello}</h1>
     </div>
   );
+}
+
+export function getStaticProps() {
+  // sanity로 부터 데이터를 가지고 온다
+  return {
+    props: {
+      hello: "world",
+    },
+  };
 }
