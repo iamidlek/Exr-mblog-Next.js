8137c034b0b7e31d586a50e02ec1622a2fdeb9df
iamidlek <iamidlek@naver.com>
2021-12-30 17:08:01 +0900

    만들어 놓은 데이터 불러오기 및 확인

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import styles from "../styles/Home.module.css";
 import sanityClient from "@sanity/client";
 
-export default function Home({ hello }) {
+export default function Home({ home, posts }) {
+  console.log(home);
+  console.log(posts);
   return (
     <div className={styles.container}>
-      <h1>Blog Home {hello}</h1>
+      <h1>Blog Home</h1>
     </div>
   );
 }
@@ -20,11 +22,37 @@ export async function getStaticProps() {
   const home = await client.fetch(
     `*[_type == 'home'][0]{'mainPostUrl': mainPost -> slug.current}`
   );
-  // 터미널에서 찍어보면
-  // console.log(data);
+
+  const posts = await client.fetch(
+    `*[_type == 'post']{
+      title, 
+      subtitle, 
+      createdAt, 
+      'content': content[]{
+        ...,
+        ...select(_type == 'imageGallery' => {'images': images[]{..., 'url': asset -> url}})
+      },
+      'slug': slug.current,
+      'thumbnail': {
+        'alt': thumbnail.alt,
+        'imageUrl': thumbnail.asset -> url
+      },
+      'author': author -> {
+        name,
+        role,
+        'image': image.asset -> url
+      },
+      'tag': tag -> {
+        title,
+        'slug': slug.current
+      }
+    }`
+  );
+
   return {
     props: {
       home,
+      posts,
     },
   };
 }
