dc5b21033524e6b220e18893c4e30324274f6b22
iamidlek <iamidlek@naver.com>
2021-12-30 17:06:00 +0900

    sanity와 연결해서 가져오기

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styles from "../styles/Home.module.css";
+import sanityClient from "@sanity/client";
 
 export default function Home({ hello }) {
   return (
@@ -8,11 +9,22 @@ export default function Home({ hello }) {
   );
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
   // sanity로 부터 데이터를 가지고 온다
+  const client = sanityClient({
+    dataset: "production",
+    projectId: "gvddgzfs",
+    useCdn: process.env.NODE_ENV === "production",
+  });
+
+  const home = await client.fetch(
+    `*[_type == 'home'][0]{'mainPostUrl': mainPost -> slug.current}`
+  );
+  // 터미널에서 찍어보면
+  // console.log(data);
   return {
     props: {
-      hello: "world",
+      home,
     },
   };
 }
